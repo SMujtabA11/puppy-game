185543aded9033c91f6c22b448ea2b5879572d8d
SMujtabA11 <159699054+SMujtabA11@users.noreply.github.com>
2024-10-19 22:34:30 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
-var Pname = prompt("enter your pet name");
-var type = prompt("What type of pet you have");
-var age = +prompt("What is age of your pet");
-var happiness = +prompt("How much your pet happiness level");
-var hunger = +prompt("How much your pet hunger level");
-var userInput = +prompt("What you want with your pet\n1-feed\n2-play\n3-age");
+var puppyName = prompt("Enter your pet name");
+var type = prompt("What type of pet do you have?");
+var age = +prompt("What is the age of your pet?");
+var happiness = +prompt("How happy is your pet on a scale of 0-100?");
+var hunger = +prompt("What is your pet's hunger level on a scale of 0-100?");
+var userInput = +prompt("What would you like to do with your pet?\n1 - Feed\n2 - Play\n3 - Age");
+
 var pet = {
-    Pname: Pname,
+    Pname: puppyName,
     type: type,
     age: age,
     happiness: happiness,
     hunger: hunger,
-    feed:function(){
-        if(this.hunger <20){
+    feed: function() {
+        if (this.hunger < 20) {
             alert("Your pet's hunger level is " + this.hunger + ". They are not hungry!");
-        }
-        else{
-            this.hunger = this.hunger-20;
-            alert("You fed Mela! Hunger level is now " + this.hunger);
+        } else {
+            this.hunger -= 20;
+            alert(`You fed ${this.Pname}. Hunger level is now ` + this.hunger);
         }
     },
-    play:function(){
-        if(this.hunger >100){
+    play: function() {
+        if (this.hunger > 100) {
             alert("Your pet is too hungry to play! Happiness level is " + this.happiness);
-        }
-        else{
-            alert("You played with Mela! Happiness level is now " + this.happiness);
+        } else {
+            this.happiness += 20; 
+            alert(`You played with ${this.Pname}. Happiness level is now ` + this.happiness);
         }
     },
-    agePet:function(){
-       this.age = this.age+1;
-       this.happiness = this.happiness-5;
-       this.hunger = this.hunger+10;
-       alert("Mela is now " + this.age + " years old! Happiness level is now " + this.happiness + " and hunger level is " + this.hunger);
-
+    agePet: function() {
+        this.age += 1;
+        this.happiness -= 5;
+        this.hunger += 10;
+        alert(`${this.Pname} is now ` + this.age + " years old! Happiness level is now " + this.happiness + " and hunger level is " + this.hunger);
     }
-}
+};
 
-if (userInput === 1){
+if (userInput === 1) {
     pet.feed();
-}else if(userInput === 2)
-{
+} else if (userInput === 2) {
     pet.play();
-}
-else if (userInput === 3) {
+} else if (userInput === 3) {
     pet.agePet();
-} else {
-    alert("Invalid input. Please enter 1, 2, or 3.");
-}
+} 
